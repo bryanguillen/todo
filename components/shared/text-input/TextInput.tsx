aa2f371c1ad7cfd3fa9c53281234587635b026db
bryg217 <bryan.guillen217@gmail.com>
2022-01-10 13:25:50 -0500

    refactor: update component interface

--- components/shared/text-input/TextInput.tsx
+++ components/shared/text-input/TextInput.tsx
@@ -5,7 +5,7 @@ import textInputStyles from './TextInput.module.css'
 /**
  * Text input component used as a wrapper to add base styles.
  */
-const TextInput = (props: React.HTMLAttributes<HTMLElement>) => {
+const TextInput: React.FC<React.HTMLProps<HTMLInputElement>> = (props) => {
   return (
     <input {...props} className={`${props.className ? props.className : ''} ${textInputStyles.base}`}/>
   )
