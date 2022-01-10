ac265d992bb5b73d7748fb80c1d2dbfda23d4346
bryg217 <bryan.guillen217@gmail.com>
2022-01-10 16:28:06 -0500

    feat: add support for logged in state

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,22 @@
+import React, { useState, useEffect } from 'react'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useRouter } from 'next/dist/client/router'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const router = useRouter()
+
+  const [loggedIn, setLoggedIn] = useState(false)
+
+  useEffect(() => {
+    if (loggedIn) {
+      router.push('/home')
+    }
+  }, [loggedIn])
+
+  return (
+    <Component {...pageProps} loggedIn={loggedIn} setLoggedIn={setLoggedIn} />
+  )
 }
 
 export default MyApp
